b5f4138bca921e7fd225259c0f7a84d9a430e2bb
shukurali <shukuralijob@gmail.com>
2024-04-17 12:34:25 +0500

    remove(cache:redis)

--- blocklist/models.py
+++ blocklist/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 # Create your models here.
 class BlackIps(models.Model):
-    ip = models.CharField(max_length=15)
+    ip = models.CharField(max_length=15, unique=True)
     reason = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    request_count = models.IntegerField(default=0)
     def __str__(self):
         return self.ip
 
--- core/middlewares.py
+++ core/middlewares.py
@@ -17,17 +17,19 @@ class DDOSMiddleware(MiddlewareMixin):
         ip_address = request.META.get('REMOTE_ADDR')
         print("ip address: ", ip_address)
         # IP-manzil qora ro'yxatga kiritilganligini tekshiring
-        if cache.get(f'ddos:blacklist:{ip_address}') or BlackIps.objects.filter(ip=ip_address, is_active=True).exists():
+        ip_address = BlackIps.objects.get(ip=ip_address, is_active=True)
+        if ip_address:
             return HttpResponseForbidden("IP manzilingiz shubhali faoliyat tufayli bloklandi.")
 
         # IP manzili uchun so'rovlar sonini oshiring
-        request_count = cache.get(f'ddos:request_count:{ip_address}', 0)
-        request_count += 1
-        cache.set(f'ddos:request_count:{ip_address}', request_count, timeout=30)
+
+        ip_address.request_count += 1
+        ip_address.save(update_fields=['request_count'])
+
         # Tarif chegarasi chegarasidan oshib ketganligini tekshiring
-        if request_count > rate_limit_threshold:
+        if ip_address.request_count > rate_limit_threshold:
             # IP-manzilni qora ro'yxatga qo'shing
-            cache.set(f'ddos:blacklist:{ip_address}', True, timeout=60)  # IP address 5 soatga bloklangandi
+            # IP address 5 soatga bloklangandi
             BlackIps.objects.create(ip=ip_address, reason='DDOS')
             return HttpResponseForbidden("Haddan tashqari so'rovlar tufayli IP-manzilingiz bloklandi")
         response = self.get_response(request)
