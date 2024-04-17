30cc650e937cf713af0de7ad62a7614afc2811b4
shukurali <shukuralijob@gmail.com>
2024-04-17 12:39:01 +0500

    blogkip model

--- blocklist/models.py
+++ blocklist/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class BlackIps(models.Model):
     ip = models.CharField(max_length=15, unique=True)
     reason = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=False)
+    blocked = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     request_count = models.IntegerField(default=0)
     def __str__(self):
--- core/middlewares.py
+++ core/middlewares.py
@@ -17,20 +17,21 @@ class DDOSMiddleware(MiddlewareMixin):
         ip_address = request.META.get('REMOTE_ADDR')
         print("ip address: ", ip_address)
         # IP-manzil qora ro'yxatga kiritilganligini tekshiring
-        ip_address = BlackIps.objects.get(ip=ip_address, is_active=True)
-        if ip_address:
+        ip_address_obj = BlackIps.objects.get(ip=ip_address, is_active=True)
+        if ip_address_obj:
             return HttpResponseForbidden("IP manzilingiz shubhali faoliyat tufayli bloklandi.")
 
         # IP manzili uchun so'rovlar sonini oshiring
-
-        ip_address.request_count += 1
-        ip_address.save(update_fields=['request_count'])
+        new_ip_address = BlackIps.objects.create(ip=ip_address, reason='DDOS')
+        new_ip_address.request_count += 1
+        new_ip_address.save(update_fields=['request_count'])
 
         # Tarif chegarasi chegarasidan oshib ketganligini tekshiring
         if ip_address.request_count > rate_limit_threshold:
             # IP-manzilni qora ro'yxatga qo'shing
             # IP address 5 soatga bloklangandi
-            BlackIps.objects.create(ip=ip_address, reason='DDOS')
+            new_ip_address.blocked = True
+            new_ip_address.save()
             return HttpResponseForbidden("Haddan tashqari so'rovlar tufayli IP-manzilingiz bloklandi")
         response = self.get_response(request)
         return response
