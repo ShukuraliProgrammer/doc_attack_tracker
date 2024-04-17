d697f2ad5a0a6a42aa4762b9a7f5c71b0cd2ce48
shukurali <shukuralijob@gmail.com>
2024-04-17 12:12:08 +0500

    deplyoment: prepared

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,8 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
+STATICFILES_DIRS = ('/home/shukurde/shukurdev.uz/django/static_files', )
+STATIC_ROOT = '/home/shukurde/shukurdev.uz/django/static'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = '/home/shukurde/shukurdev.uz/django/media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -134,3 +137,4 @@ CACHES = {
         }
     }
 }
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
